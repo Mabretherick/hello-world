f99884aa257424e8c04bfc458bdad772228bce30
Mabretherick <mabretherick@gmail.com>
2015-08-31 14:50:23 -0500

    First main. Along with all typos that most likely are there.

---
+++ helloWorld/Project1/main.cpp
@@ -0,0 +1,11 @@
+/*
+Hello World
+
+My refresher in simple coding that I probably shouldn't have forgotten. At least theres always google.
+*/
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+	return 0;
+};
